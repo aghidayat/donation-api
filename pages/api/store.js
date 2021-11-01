86bdcf53317c31b2e52b8d819d623c15d8c842a2
Adi Gunawan Hidayat <adigunawanhidayat29@gmail.com>
2021-11-01 14:53:03 +0700

    init

---
+++ pages/api/store.js
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+    // if (req.method !== 'POST') {
+    //     res.status(405).send({ message: 'Only POST requests allowed' });
+    //     return;
+    // }
+
+    const request = req.body;
+
+    const resp = await fetch('https://api.xendit.co/v2/invoices', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Basic ' +
+                Buffer.from(
+                    'xnd_development_q9F8uv2g3IkUKe2tGqgxZRmBipufboFnNiIyvB1zi2XGla4aU5vcUyjFy9nSDaHa' +
+                    ':'
+                ).toString('base64'),
+        },
+        body: JSON.stringify({
+            external_id: request.external_id,
+            amount: request.amount,
+            payer_email: request.payer_email,
+            description: request.description,
+        }),
+    });
+    const data = await resp.json();
+    res.status(200).json(data);
+}
